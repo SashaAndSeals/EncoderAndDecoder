6ccbb790edf75e521665537270146c455f493cbf
SashaAndSeals <146691256+SashaAndSeals@users.noreply.github.com>
2024-11-18 18:10:31 -0800

    Add files via upload

---
+++ it.py
@@ -0,0 +1,25 @@
+import math
+letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,?!@#$%^&*()[]{}\|/<>+-=`~:;' "
+c = math.floor(len(letters)/2)
+def encode(a):
+    out = ""
+    c = math.floor(len(letters)/2)
+    for b in a:
+        out += letters[(letters.index(b)-c)%len(letters)]
+        c += letters.index(b)
+    return out
+def decode(a):
+    out = ""
+    c = math.floor(len(letters)/2)
+    for b in a:
+        out += letters[(letters.index(b)+c)%len(letters)]
+        c += letters.index(b)+c
+    return out
+while 1:
+    proc = input("ENCODE OR DECODE: ").lower()
+    while proc != "encode" and proc != "decode":
+        proc = input("ENCODE OR DECODE: ").lower()
+    if proc == "encode":
+        print(encode(input("STRING TO ENCODE: ")))
+    elif proc == "decode":
+        print(decode(input("STRING TO DECODE: ")))
